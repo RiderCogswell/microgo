90605025e2aa667a0c25ac8a1f293cede8f15249
Ridercogswell <ridercogswell@gmail.com>
2022-10-10 23:27:09 -0700

    comment

--- client/cmd/web/main.go
+++ client/cmd/web/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// handler to render the test page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
@@ -18,23 +19,25 @@ func main() {
 		log.Panic(err)
 	}
 }
-
+// arg t for template
 func render(w http.ResponseWriter, t string) {
 
+	// define a slice of strings holding the paths to the pages
 	partials := []string{
 		"./cmd/web/templates/base.layout.gohtml",
 		"./cmd/web/templates/header.partial.gohtml",
 		"./cmd/web/templates/footer.partial.gohtml",
 	}
 
+	// append our templates and the passed in t arg
 	var templateSlice []string
 	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
 
-	for _, x := range partials {
+	for _, x := range partials { // loop through and 
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFiles(templateSlice...) // parse the slice of templates using a variadic parameter
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
